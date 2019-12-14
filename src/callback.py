3e49a0f6fb5e438ba1e9cd718fbfaefe169b7b22
Syed Hussain Ather <shussainather@gmail.com>
2019-12-13 21:42:12 -0800

    commetn

--- src/callback.py
+++ src/callback.py
@@ -2,9 +2,23 @@ import csv
 
 """
 This callback class and function scrapes the country data
-and saves it in a readable csv format.
+and saves it in a readable csv format. The callback lets you
+handle multiple websites by using the function after certain events
+(such as after the webpages of interest have been downloaded).
+
+In this case, the callback will take a url and html parameters and return
+a list of more URLs to crawl.
 """
 
 # Move up a directory to get to the "scrape" directory.
 while os.getcwd().split("\")[-1] != "scrape":
     os.chdir("..") # Move up one directory.
+
+class ScrapeCallBack:
+    """
+    A class to add a scrape callback parameter
+    """
+    def __init__(self):
+        """
+        Initialize the writer and fields.
+        """
