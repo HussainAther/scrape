3f8468ac2777250d6ac4aae96c16face75a89f8a
Syed Hussain Ather <shussainather@gmail.com>
2019-12-13 20:41:40 -0800

    print

--- src/bs4.py
+++ src/bs4.py
@@ -7,3 +7,11 @@ Sample code to test out Beautiful Soup.
 # Fix some broken HTML with the Beautiful Soup parser.
 brokenhtml = "<ul class=country><li>Area<li>Population</ul>"
 
+# Parse the HTML.
+soup = BeautifulSoup(brokenhtml, "html.parser")
+
+# Make it pretty. 
+fixedhtml = soup.prettify()
+
+# Print the results.
+print(fixedhtml)
