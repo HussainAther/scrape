453aecd450d164361d8aa45ecae2a4d7e66cebef
Syed Hussain Ather <shussainather@gmail.com>
2019-12-13 18:21:46 -0800

    make directory output

--- src/results.py
+++ src/results.py
@@ -17,9 +17,13 @@ for url in sys.argv[1]:
 while os.getcwd().split("\")[-1] != "scrape":
     os.chdir("..") # Move up one directory.
 
+# Make the output directory if it isn't there already.
+if not os.path.isdir("/output/results"):
+    os.mkdir("output/results")
+
 numiter = 1000 # number of iterations (times) to test each scraper
 
 for url in urllist:
     html = downloadurl(url)
     for line in html:
-        with open("
+        with open("output/
