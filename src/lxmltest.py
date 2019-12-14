2e142561d3e3d70db1907a0d42709046568da243
Syed Hussain Ather <shussainather@gmail.com>
2019-12-13 20:57:42 -0800

    build tree

--- src/lxmltest.py
+++ src/lxmltest.py
@@ -1,1 +1,13 @@
 import lxml.html
+
+"""
+Parse some broken HTML text with lxml.
+"""
+
+# Broken HTMl
+brokenhtml = "<ul class=country><li>Area<li>Population</ul>"
+
+# Build an lxml tree to parse the HTML.
+tree = lxml.html.fromtstring(brokenhtml)
+ 
+
