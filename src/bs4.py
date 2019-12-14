0d497afddc45895ed30b7bd2eb08eb060dce7a55
Syed Hussain Ather <shussainather@gmail.com>
2019-12-13 20:40:47 -0800

    init

---
+++ src/bs4.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+
+"""
+Sample code to test out Beautiful Soup.
+"""
+
+# Fix some broken HTML with the Beautiful Soup parser.
+brokenhtml = "<ul class=country><li>Area<li>Population</ul>"
+
