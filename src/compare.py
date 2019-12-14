217a8f5b384d4acc10223901bd4531fc4553a59f
Syed Hussain Ather <shussainather@gmail.com>
2019-12-13 21:18:39 -0800

    bs scraper

--- src/compare.py
+++ src/compare.py
@@ -1,4 +1,7 @@
+import lxml.html
 import re
+
+from bs4 import BeautifulSoup
 
 """
 Evaluate the trade-offs of the three scraping approaches
@@ -20,3 +23,16 @@ def rescraper(html):
         # and add each finding to the results dictionary.
         results[field] = re.search('<td id="places_%s_row">.*?<td class="w2p_fw">(.*?)</td>' % field, html).groups()[0]
     return results
+
+def bsscraper(html):
+    """
+    Use BeautifulSoup to clean the HTML.
+    """
+    soup = BeautifulSoup(html, "html.parser")
+    results = {}
+    for field in fields:
+        # Same as with rescraper, but using the Beautiful Soup method.
+        results[field] = soup.find("table").find("tr", id="places_%s_row" % field).find("td", class_="w2p_fw").text
+    return results
+
+
