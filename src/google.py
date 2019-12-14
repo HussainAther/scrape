835b408b4ba2c188a7cb224dc3a7673884cd963f
Syed Hussain Ather <shussainather@gmail.com>
2019-12-14 00:07:45 -0800

    url import semantics

--- src/google.py
+++ src/google.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import urllib
-import urlparse
 import lxml.html
 import sys
 
@@ -19,21 +17,19 @@ def search(keyword):
     Google search for a keyword.
     """
     D = Downloader()
-    url = 'https://www.google.com/search?q=' + urllib.quote_plus(keyword)
+    url = "https://www.google.com/search?q=" + ul.quote_plus(keyword)
     html = D(url)
     tree = lxml.html.fromstring(html)
     links = []
-    for result in tree.cssselect('h3.r a'):
-        link = result.get('href')
-        qs = urlparse.urlparse(link).query
-        links.extend(urlparse.parse_qs(qs).get('q', []))
+    for result in tree.cssselect("h3.r a"):
+        link = result.get("href")
+        qs = ulp.urlparse(link).query
+        links.extend(ulp.parse_qs(qs).get("q", []))
     return links
-
     
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         keyword = sys.argv[1]
     except IndexError:
-        keyword = 'test'
-    print search(keyword)
-
+        keyword = "test"
+    print(search(keyword))
