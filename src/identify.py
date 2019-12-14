c3bfeff2053718e33fad03e1afa4a0cd4f4f7670
Syed Hussain Ather <shussainather@gmail.com>
2019-12-13 18:32:04 -0800

    extract line

--- src/identify.py
+++ src/identify.py
@@ -6,5 +6,6 @@ Identify the type of technology that was used to build a website.
 
 # Extract each URL from the list of URLs
 urllist = [] # list of URLs from the input
-for url in sys.argv[1]:
-    urlllist.append(url.replace("\n", ""))
+with open(sys.argv[1], "r") as file:
+    for line in file:
+        urlllist.append(url.replace("\n", ""))
