bc2e073cb0e2c71ae805a8ae54bc3f8f0a04f41a
Syed Hussain Ather <shussainather@gmail.com>
2019-12-13 17:07:26 -0800

    sys

--- src/results.py
+++ src/results.py
@@ -1,7 +1,11 @@
+import sys
 import time
+
+from download import downloadurl
 
 """
 Scrape the results of downloaded webpages.
 """
 
 numiter = 1000 # number of iterations (times) to test each scraper
+html = downloadurl(sys.argv[1])
