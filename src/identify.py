1fc89be82a10912e841a25a244ef127a6338f056
Syed Hussain Ather <shussainather@gmail.com>
2019-12-13 18:33:32 -0800

    parse each url

--- src/identify.py
+++ src/identify.py
@@ -9,3 +9,9 @@ urllist = [] # list of URLs from the input
 with open(sys.argv[1], "r") as file:
     for line in file:
         urlllist.append(url.replace("\n", ""))
+
+# Parse each URL for the technology behind it.
+# This should give info about the frameworks, programming languages,
+# and servers used in building each website.
+for url in urllist:
+    builtwithparse(url)
