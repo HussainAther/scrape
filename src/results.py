6cb921c6eb0b8e434f9a403dfb920acfe6226232
Syed Hussain Ather <shussainather@gmail.com>
2019-12-13 18:20:14 -0800

    os

--- src/results.py
+++ src/results.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 
@@ -7,5 +8,18 @@ from download import downloadurl
 Scrape the results of downloaded webpages.
 """
 
+# Extract each URL from the list of URLs
+urllist = [] # list of URLs from the input
+for url in sys.argv[1]:
+    urlllist.append(url.replace("\n", ""))
+
+# Move up a directory to get to the "scrape" directory.
+while os.getcwd().split("\")[-1] != "scrape":
+    os.chdir("..") # Move up one directory.
+
 numiter = 1000 # number of iterations (times) to test each scraper
-html = downloadurl(sys.argv[1])
+
+for url in urllist:
+    html = downloadurl(url)
+    for line in html:
+        with open("
