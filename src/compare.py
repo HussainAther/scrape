686ed5490f86c17631039263c7e3717dba59d634
Syed Hussain Ather <shussainather@gmail.com>
2019-12-13 21:09:07 -0800

    fields

--- src/compare.py
+++ src/compare.py
@@ -1,4 +1,11 @@
+import re
+
 """
 Evaluate the trade-offs of the three scraping approaches
-by comparing their relative efficiency.
+by comparing their relative efficiency using Firebug.
 """
+
+fields = ("area", "population", "iso", "country", "capital",
+          "continent", "tld", "currency_code", "currency_name",
+          "phone", "postal_code_format", "postal_code_regex", 
+          "languages", "neighbors")
