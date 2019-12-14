95ea5ebc37cf2d7d45f668faaf3ce3b1a0b586b7
Syed Hussain Ather <shussainather@gmail.com>
2019-12-13 19:52:35 -0800

    classtag

--- src/regex.py
+++ src/regex.py
@@ -1,10 +1,11 @@
 import re
+import sys
 
 from download import downloadurl 
 
 """
 This script uses regular expressions to match the contents of a <td>
-element in the HTML of a webpage.
+class tag element in the HTML of a webpage.
 """
 
 # Extract each URL from the list of URLs
@@ -13,7 +14,10 @@ with open(sys.argv[1], "r") as file:
     for line in file:
         urlllist.append(url.replace("\n", ""))
 
+# Introduce a class tag to search for.
+classtag = sys.argv[2]
+
 # Search through the HTML of each URL.
 for url in urllist:
     html = downloadurl(url) # Extract the HTML.
-    re.findall('<td class="2p_fw">(.*?)</td>, html)
+    re.findall('<td class=" + str(classtag) + ">(.*?)</td>', html)
