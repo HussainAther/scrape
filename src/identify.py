4f20c9aac38f4b93a8ef094d683d6416eb19ed9d
Syed Hussain Ather <shussainather@gmail.com>
2019-12-13 18:28:44 -0800

    url list

--- src/identify.py
+++ src/identify.py
@@ -3,3 +3,8 @@ import builtwidth
 """
 Identify the type of technology that was used to build a website. 
 """
+
+# Extract each URL from the list of URLs
+urllist = [] # list of URLs from the input
+for url in sys.argv[1]:
+    urlllist.append(url.replace("\n", ""))
