7516116f3e493daf319a7b9add0efbf7ce441805
Syed Hussain Ather <shussainather@gmail.com>
2019-12-13 21:11:34 -0800

    re.search

--- src/compare.py
+++ src/compare.py
@@ -8,4 +8,15 @@ by comparing their relative efficiency using Firebug.
 fields = ("area", "population", "iso", "country", "capital",
           "continent", "tld", "currency_code", "currency_name",
           "phone", "postal_code_format", "postal_code_regex", 
-          "languages", "neighbors")
+          "languages", "neighbours")
+
+def rescraper(html):
+    """
+    Use regex to scrape.
+    """
+    results = {}
+    for field in fields:
+        # For each field, search the HTML using the following regex,
+        # and add each finding to the results dictionary.
+        results[field] = re.search('<td id="places_%s_row">.*?<td class="w2p_fw">(.*?)</td>' % field, html).groups()[0]
+    return results
